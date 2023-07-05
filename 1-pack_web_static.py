80138ec1492ca57554171bd3056649670c7222e7
AtaeTL <ataetlidii@gmail.com>
2023-07-05 09:47:49 -0700

    Update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-""" Fabric script to create tarball"""
-
-import tarfile
-import os
+"""
+Fabric script that Generates a .tgz archive
+"""
+from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
-    """ Creates tar archive"""
-    savedir = "versions/"
-    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
-    if not os.path.exists(savedir):
-        os.mkdir(savedir)
-    with tarfile.open(savedir + filename, "w:gz") as tar:
-        tar.add("web_static", arcname=os.path.basename("web_static"))
-    if os.path.exists(savedir + filename):
-        return savedir + filename
-    else:
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+    except:
         return None
